6f441b4d88cca2afd378087fff06d32dacca92c7
TddyJ <157575715+TddyJ@users.noreply.github.com>
2024-06-09 19:28:23 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from io import BytesIO
 # Function to load the model
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model = tf.keras.models.load_model('base_model.h5')
+    model = tf.keras.models.load_model('revised_base_model_v2.h5')
     return model
 
 @st.cache
